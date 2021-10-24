a5fcafe3febdb12c4ecd4ab12af621baa68e5144
Ocean <1223662448@qq.com>
2021-10-24 21:19:06 +0800

    修改 GitTest.java 测试合并 -- 冲突合并 ——hot-fix

--- src/main/java/com/study/seckill/GitTest.java
+++ src/main/java/com/study/seckill/GitTest.java
@@ -13,6 +13,7 @@ public class GitTest {
         System.out.println("hello Test");
         System.out.println("merge test!");
         System.out.println("Hello Git");
+        System.out.println("Test conflict merge -- hot-fix");
     }
 
 }
