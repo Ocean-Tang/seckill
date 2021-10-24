c55a6905930a67118666c18bed28fa27463e51d6
Ocean <1223662448@qq.com>
2021-10-24 17:52:01 +0800

    测试 git 1，创建GitTest.java

---
+++ src/main/java/com/study/seckill/GitTest.java
@@ -0,0 +1,15 @@
+package com.study.seckill;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author 黄灿杰
+ * @date 2021/10/24
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+
+}
